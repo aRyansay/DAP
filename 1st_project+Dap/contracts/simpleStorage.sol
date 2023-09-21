56f623f65e5278b8353fdaac70a05a61730b3ed3
ARYAN SHARMA <aryansharma2216@gmail.com>
2023-09-22 03:15:22 +0530

    first commit

---
+++ 1st_project+Dap/contracts/simpleStorage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.13;
+
+ contract simple{
+    uint public count =10;
+
+function increament() public{
+    count++;
+}
+function  decrement() public{
+    count--;
+}
+
+
+ }
